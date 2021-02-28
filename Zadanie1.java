8d121130d0353ddb165ade7c25cdff94d9468fab
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 17:57:09 +0100

    Delete Zadanie1.java

--- Zadanie1.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Main{
-    public static void zadanie1(String[] args){
-
-        for (int liczba = 10; liczba <= 20;liczba++) {
-            System.out.println(liczba);
-        }}}
