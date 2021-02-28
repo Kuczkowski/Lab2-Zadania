d2c3c0ecbadef0d586c892356eecc0d563e8cf26
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 18:18:31 +0100

    Add files via upload

---
+++ Zadanie4.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Zadanie4{
+    public static void main(String[] args){
+        for (int liczba=100;liczba >= 1;liczba--)
+        {
+            System.out.println(liczba);
+        } } }
