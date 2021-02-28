ff1e36f8873a3aa09f5ee4d417f52b195ef96d13
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 18:15:06 +0100

    Add files via upload

---
+++ Zadanie3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Zadanie3{
+    public static void main(String[] args){
+        int liczba=-10;
+        while (liczba <= 40)
+        {
+            if (liczba % 2 != 0)
+                System.out.println(liczba);
+            liczba++;
+
+        } } }
