14688441e42e5683f578f747eb88c90801be7f08
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 17:58:37 +0100

    Add files via upload

---
+++ Zadanie2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+
+public class Zadanie2{
+    public static void main(String[] args){
+
+        for (int liczba = -10; liczba <= 40;liczba++) {
+            if (liczba % 2 != 0)
+                System.out.println(liczba);
+        }}}
