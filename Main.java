5dcb07925c09970509df060b578b869425df7404
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 17:50:04 +0100

    Zadanie2

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 package com.company;
+import java.util.Scanner;
 
 
 public class Main{
     public static void main(String[] args){
 
-        for (int liczba = -10; liczba <= 40;liczba++)
-        {
+        for (int liczba = -10; liczba <= 40;liczba++) {
             if (liczba % 2 != 0)
-                System.out.println(liczba);}}}
+                System.out.println(liczba);
+        }}}
