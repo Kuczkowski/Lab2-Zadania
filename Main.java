71790512a4fc9f1ada068ab2caa21f796a182dc7
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 17:46:54 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 package com.company;
-import java.util.Scanner;
 
 
 public class Main{
-    public static void zadanie1(String[] args){
-        int liczba=10;
-        while(liczba<=20){
-            System.out.println(liczba);
-            liczba++;
+    public static void main(String[] args){
 
-        } } }
+        for (int liczba = -10; liczba <= 40;liczba++)
+        {
+            if (liczba % 2 != 0)
+                System.out.println(liczba);}}}
