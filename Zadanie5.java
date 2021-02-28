b575bdf6f7069303310992b09c5a04497193e563
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 18:20:02 +0100

    Add files via upload

---
+++ Zadanie5.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Zadanie5{
+    public static void main(String[] args){
+        for (int liczba=1;liczba <= 100;liczba++)
+        {
+            if (liczba % 8 ==0)
+                System.out.println(liczba);
+        } } }
