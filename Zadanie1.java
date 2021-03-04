30c165e697417d68cab9ce876041649df1ab9711
Kuczok <79452575+Kuczkowski@users.noreply.github.com>
2021-03-04 13:04:15 +0100

    Update Zadanie1.java

--- Zadanie1.java
+++ Zadanie1.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class Zadanie1{
     public static void zadanie1(String[] args){
-
-        for (int liczba = 10; liczba <= 20;liczba++) {
-            System.out.println(liczba);
-        }}}
+        int a=10;
+	while (a<=20){
+	    System.out.println(a);
+		a++;
+    } } }
