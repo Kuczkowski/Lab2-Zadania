729015aebc5245e5ce72a22f52d0a5ff5a519ee8
Kuczok <79452575+Kuczkowski@users.noreply.github.com>
2021-03-04 13:08:26 +0100

    Update Zadanie3.java

--- Zadanie3.java
+++ Zadanie3.java
@@ -2,8 +2,11 @@ package com.company;
 
 public class Zadanie3{
     public static void main(String[] args) {
-	int a=10;
-	while (a<=20){
-	    System.out.println(a);
-		a++;
-    } } }
+	int liczba=-10;
+        while (liczba <= 40)
+        {
+            if (liczba % 2 != 0)
+                System.out.println(liczba);
+            liczba++;
+
+        } } }
